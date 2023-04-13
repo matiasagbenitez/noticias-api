4cf7cc5a87ca0321c8af2776b97405c350e807a1
Matías <matiasagbenitez@gmail.com>
2023-04-13 15:04:18 -0300

    context y detectando la categoría seleccionada

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import { Container, Grid, Typography } from "@mui/material";
+import { Formulario } from "./components/Formulario";
+import { NoticiasProvider } from "./context/NoticiasProvider";
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <header>
-        <Typography align="center" marginY={5} component="h1" variant="h2" fontWeight={600}>
-          Portal de Noticias
-        </Typography>
-      </header>
-    </Container>
-    )
+    <NoticiasProvider>
+      <Container maxWidth="sm">
+        <header>
+          <Typography
+            align="center"
+            marginY={5}
+            component="h1"
+            variant="h2"
+            fontWeight={600}
+          >
+            Portal de Noticias
+          </Typography>
+        </header>
+
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12}>
+            <Formulario />
+          </Grid>
+        </Grid>
+      </Container>
+    </NoticiasProvider>
+  );
 }
 
 export default App;
