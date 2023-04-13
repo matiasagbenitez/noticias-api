fb9fccbf54542527cba77e3c98eacd65b65cf303
Matías <matiasagbenitez@gmail.com>
2023-04-13 14:44:01 -0300

    commit inicial. instalación de MUI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Container, Grid, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <header>
+        <Typography align="center" marginY={5} component="h1" variant="h2" fontWeight={600}>
+          Portal de Noticias
+        </Typography>
+      </header>
+    </Container>
+    )
+}
+
+export default App;
